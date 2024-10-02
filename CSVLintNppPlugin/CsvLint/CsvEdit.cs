adc8017a23d6b80df02d734542fafffc40f30cbe
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2024-10-02 10:26:54 -0700

    remove unused "using" it was auto-inserted by Visual Studio in an earlier commit

--- CSVLintNppPlugin/CsvLint/CsvEdit.cs
+++ CSVLintNppPlugin/CsvLint/CsvEdit.cs
@@ -11,7 +11,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Policy;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
